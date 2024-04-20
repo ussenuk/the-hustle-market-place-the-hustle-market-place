e84fae4fa579cba73efd2c8dc5c533f9bde90bfb
Leon <leon.muriuki@student.moringaschool.com>
2024-04-20 22:10:03 +0300

    Added serializer rules

--- server/app.py
+++ server/app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from config import app, db, CORS
-
-
 from models import Customer, ServiceProvider, Payment, Review, Booking, Service
 
-
-
 # Home route
-@app.route('/')
-def home():
-    return 'Welcome to the Home Page'
+class Home(Resource):
+
+    def get(self):
+
+        response_dict = {
+            "message": "Welcome to the Home Page",
+        }
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(Home, '/')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
