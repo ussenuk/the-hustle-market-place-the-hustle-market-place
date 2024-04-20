cd1d30fc595a0721657cb75d94bcf88830329862
Kevin <kevin.kiseli@student.moringaschool.com>
2024-04-20 09:25:52 +0300

    Updated to match development

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+
+
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+
+
+const Navigation = () => {
+  return (
+    <div className="nav" id="nav">
+      <Link to="/hands">Our Services</Link>
+      <Link to="/about">About Us</Link>
+      <Link to="/feedback">Feedback</Link>
+      <Link to="/register">Register</Link>
+    </div>
+  );
+};
+
+export default Navigation;
